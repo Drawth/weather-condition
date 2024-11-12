2936b641750d4a3d206f8525a8b7536fbe3b922c
Alper <2022212084@cumhuriyet.edu.tr>
2024-11-12 14:45:52 +0300

    animation

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
 const url = "http://api.openweathermap.org/geo/1.0/direct?";
 const key = "c460915944b7ec03852faba5211f506f";
 
+const showLoading = () =>
+  (document.getElementById("loading").style.display = "flex");
+const hideLoading = () =>
+  (document.getElementById("loading").style.display = "none");
+
 const getLatLon = async (cityName) => {
   const link = `${url}q=${cityName}&appid=${key}`;
-
-  await fetch(link)
-    .then((weather) => {
-      return weather.json();
-    })
-    .then(getResult);
+  try {
+    showLoading();
+    await fetch(link)
+      .then((weather) => {
+        return weather.json();
+      })
+      .then(getResult);
+  } catch (error) {
+    hideLoading();
+    alert(`Bir hata oluştu: ${error.message}`);
+  } finally {
+    hideLoading();
+  }
 };
 
 const getResult = async (result) => {
   if (result == "") {
     alert("Geçerli bir şehir giriniz");
+    return;
   }
   const query = `https://api.openweathermap.org/data/2.5/weather?lat=${result[0].lat}&lon=${result[0].lon}&appid=${key}`;
 
-  await fetch(query)
-    .then((weather) => {
-      return weather.json();
-    })
-    .then(displayResult);
+  try {
+    await fetch(query)
+      .then((weather) => {
+        return weather.json();
+      })
+      .then(displayResult);
+  } catch (error) {
+    alert(`Bir hata oluştu: ${error.message}`);
+  }
+};
+
+const convertTheKelvin = (kelvin) => {
+  return `${Math.round(kelvin) - 273}°C`;
+};
+
+const convertTheMinMaxKelvin = (minKelvin, maxKelvin) => {
+  return `${Math.round(minKelvin - 273)}°C  / ${Math.round(maxKelvin - 273)}°C`;
 };
 
 const displayResult = (result) => {
@@ -29,15 +54,16 @@ const displayResult = (result) => {
   city.innerHTML = `${result.name},${result.sys.country}`;
 
   let temp = document.querySelector(".temp");
-  temp.innerHTML = `${Math.round(result.main.temp) - 273}°C`;
+  temp.innerHTML = convertTheKelvin(result.main.temp);
 
   let desc = document.querySelector(".desc");
   desc.innerHTML = result.weather[0].description;
 
   let minmax = document.querySelector(".minmax");
-  minmax.innerHTML = `${Math.round(
-    result.main.temp_min - 273
-  )}°C  / ${Math.round(result.main.temp_max - 273)}°C`;
+  minmax.innerHTML = convertTheMinMaxKelvin(
+    result.main.temp_min,
+    result.main.temp_max
+  );
 };
 
 const setQuery = (e) => {
